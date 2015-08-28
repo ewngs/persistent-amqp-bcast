227ad4860d88d7e9d54248681958a883fe9a6454
Peter Kovacs <kiiwep@gmail.com>
2015-08-28 14:47:03 +0200

    Pub/Sub implementation with a simple example client

--- test/test-client.js
+++ test/test-client.js
@@ -1,18 +1,45 @@
 'use strict';
 
-const broadcast = require('..')({}, 'messages');
+const broadcast = require('..')(null, // default connection is localhost
+    {
+        exchangeName: 'messages'
+        // queueName: 'shared.message.queue',
+        // queueOptions: {
+        //     exclusive: false
+        // }
+    });
 
-broadcast.on('msg.#', function (message) {
-    console.log('Message received:', message);
+broadcast.on('#', function (topic, message) {
+    console.log(`[#] Message to ${topic} received:`, message);
+});
+
+broadcast.on('msg.#', function (topic, message) {
+    console.log(`[msg.#] Message to ${topic} received:`, message);
+});
+
+broadcast.on('msg.*.yolo', function (topic, message) {
+    console.log(`[msg.*.yolo] Message to ${topic} received:`, message);
+});
+
+broadcast.on('#.yolo', function (topic, message) {
+    console.log(`[#.yolo] Message to ${topic} received:`, message);
+});
+
+broadcast.on('*.*.yolo', function (topic, message) {
+    console.log(`[*.*.yolo] Message to ${topic} received:`, message);
+});
+
+broadcast.post('msg.test.yolo', {
+    test: 'yolo'
 });
 
 process.on('SIGHUP', () => {
     console.log('HUP received. Shutdown...');
-    broadcast.shutdown();
-        // .then(() => {
-        //     console.log('Closed');
-        // })
-        // .catch(function (err) {
-        //     console.log(err);
-        // });
+    broadcast.shutdown()
+        .then(() => {
+            console.log('Closed');
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 });
